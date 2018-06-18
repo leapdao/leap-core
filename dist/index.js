377058c61ece8c4539cb9305cfba3a46bc2b6873
Kosta Korenkov <kosta@whoot.me>
2018-06-18 19:26:05 +0300

    expose Util

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,9 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.Outpoint = exports.Output = exports.Input = exports.Type = exports.Tx = exports.Block = exports.Period = undefined;var _period = require('./period');var _period2 = _interopRequireDefault(_period);
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.Util = exports.Outpoint = exports.Output = exports.Input = exports.Type = exports.Tx = exports.Block = exports.Period = undefined;var _period = require('./period');var _period2 = _interopRequireDefault(_period);
 var _block = require('./block');var _block2 = _interopRequireDefault(_block);
 var _transaction = require('./transaction');var _transaction2 = _interopRequireDefault(_transaction);
 var _input = require('./input');var _input2 = _interopRequireDefault(_input);
 var _output = require('./output');var _output2 = _interopRequireDefault(_output);
-var _outpoint = require('./outpoint');var _outpoint2 = _interopRequireDefault(_outpoint);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}exports.
+var _outpoint = require('./outpoint');var _outpoint2 = _interopRequireDefault(_outpoint);
+var _util = require('./util');var _util2 = _interopRequireDefault(_util);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}exports.
 
-Period = _period2.default;exports.Block = _block2.default;exports.Tx = _transaction2.default;exports.Type = _transaction.Type;exports.Input = _input2.default;exports.Output = _output2.default;exports.Outpoint = _outpoint2.default;
+Period = _period2.default;exports.Block = _block2.default;exports.Tx = _transaction2.default;exports.Type = _transaction.Type;exports.Input = _input2.default;exports.Output = _output2.default;exports.Outpoint = _outpoint2.default;exports.Util = _util2.default;
