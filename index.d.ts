7a1a5ccacae857509e53e8ad3a0bf1ed6162b4f5
Alex Lunyov <isuntc@gmail.com>
2018-08-01 16:00:25 +0800

    Extend validatorJoin and validatorLogout txs, drop validatorLeave (#15)
    * Drop validatorLeave tx
    
    * Extend validatorJoin tx with signerAddr
    
    * Typedefs update
    
    * newSigner field for validatorLogout tx
    
    * Typedefs update

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ declare module "parsec-lib" {
     EXIT = 7,
     VALIDATOR_JOIN = 8,
     VALIDATOR_LOGOUT = 9,
-    VALIDATOR_LEAVE = 10,
+    PERIOD_VOTE = 11,
   }
 
   type TransferOutputObject = {
@@ -122,9 +122,8 @@ declare module "parsec-lib" {
     public outputs: Array<Output>;
     public options?: TxOptions;
 
-    public static validatorJoin(slotId: number, tenderKey: string, eventsCount: number): Tx<Type.VALIDATOR_JOIN>;
-    public static validatorLogout(slotId: number, tenderKey: string, eventsCount: number, activationEpoch: number): Tx<Type.VALIDATOR_LOGOUT>;
-    public static validatorLeave(slotId: number, tenderKey: string, eventsCount: number): Tx<Type.VALIDATOR_LEAVE>;
+    public static validatorJoin(slotId: number, tenderKey: string, eventsCount: number, signerAddr: string): Tx<Type.VALIDATOR_JOIN>;
+    public static validatorLogout(slotId: number, tenderKey: string, eventsCount: number, activationEpoch: number, newSigner: string): Tx<Type.VALIDATOR_LOGOUT>;
     public static deposit(depositId: number, value: number, address: string, color: number): Tx<Type.DEPOSIT>;
     public static exit(input: Input): Tx<Type.EXIT>;
     public static transfer(inputs: Array<Input>, outputs: Array<Output>): Tx<Type.TRANSFER>;
