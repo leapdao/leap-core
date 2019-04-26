73a1c6e746581ca540d1ae9f52b6e7b006c52aab
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-04-26 14:54:09 +0200

    wip

--- test/helpers/web3.js
+++ test/helpers/web3.js
@@ -1,5 +1,7 @@
 import ethUtils from 'ethereumjs-util';
 
+const assert = require('assert');
+
 const fakeSign = (keys) => (dataToSign, address, cb) => {
   const sigHashBuf = ethUtils.hashPersonalMessage(Buffer.from(dataToSign.replace('0x', ''), 'hex'));
   const sig = ethUtils.ecsign(
@@ -13,9 +15,39 @@ const fakeSign = (keys) => (dataToSign, address, cb) => {
   cb(null, `0x${dataBuf.toString('hex')}`);
 };
 
+const providerSend = (keys) => async ({ method, params, from }, callback) => {
+  // would be eth_signTypedData_v1 later
+  assert.equal(method, 'eth_signTypedData');
+  // [typed data objs..] from
+  assert.equal(params.length, 2);
+
+  const typedData = params[0];
+  assert.equal(typedData.length, 1, 'expected only one typed data object for now. FIXME if we need more in the future');
+
+  const header = Buffer.from(`${typedData[0].type} ${typedData[0].name}`, 'utf8');
+  const data = ethUtils.toBuffer(typedData[0].value);
+  const headerHash = ethUtils.keccak256(header);
+  const dataHash = ethUtils.keccak256(data);
+  const finalhash = ethUtils.keccak256(Buffer.concat([headerHash, dataHash]));
+
+  const sig = ethUtils.ecsign(
+    finalhash,
+    ethUtils.toBuffer(keys[0])
+  );
+
+  const dataBuf = Buffer.alloc(65);
+
+  sig.r.copy(dataBuf, 0);
+  sig.s.copy(dataBuf, 32);
+  dataBuf.writeInt8(sig.v, 64);
+  callback(null, { result: `0x${dataBuf.toString('hex')}` });
+}
+
 export default (accounts, keys) => ({
   version: '1',
-  currentProvider: {},
+  currentProvider: {
+    send: providerSend(keys),
+  },
   eth: {
     personal: {
       sign: fakeSign(keys)
